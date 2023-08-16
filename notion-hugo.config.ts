71d9460647e4edc6c5a905f82f46a64578cf343d
kuaimeizheng <kuaimeizheng@gmail.com>
2023-08-16 17:47:55 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://uxhug.notion.site/Notion-DoIt-d4d107ea67184795b53db5ea3959ca18?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
